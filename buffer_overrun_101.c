3b7f46b25796f1096c8ce8ba865ed04a0aa89656
Tim Sell <Timothy.Sell@unisys.com>
2022-04-08 14:36:35 -0400

    create more organized buffer overrun tests

---
+++ buffer_overrun_101.c
@@ -0,0 +1,71 @@
+#include "all.h"
+
+struct test_struct
+{
+    char open_ended_array[1];
+};
+
+static void f_easy1()
+{
+    struct test_struct *p;
+    size_t bytes = 5;
+    size_t total_bytes = sizeof(struct test_struct)+(bytes-1);
+    p = (struct test_struct *)(malloc(total_bytes));
+    if (p == NULL) return;
+    strncpy(&(p->open_ended_array[0]), "hello_world", 10);
+}
+
+static void f_easy2()
+{
+    struct test_struct *p;
+    size_t bytes = 5;
+    size_t total_bytes = sizeof(struct test_struct)+(bytes-1);
+    p = (struct test_struct *)(malloc(total_bytes));
+    if (p == NULL) return;
+    strncpy_s(&(p->open_ended_array[0]), 10, "hello_world", 10);
+}
+
+static void f_easy3()
+{
+    struct test_struct *p;
+    size_t bytes = 5;
+    size_t total_bytes = sizeof(struct test_struct)+(bytes-1);
+    p = (struct test_struct *)(malloc(total_bytes));
+    if (p == NULL) return;
+    memcpy_s(&(p->open_ended_array[0]), 10, "hello_world", 10);
+}
+
+static void f_easy4()
+{
+    struct test_struct *p;
+    size_t bytes = 5;
+    size_t total_bytes = sizeof(struct test_struct)+(bytes-1);
+    p = (struct test_struct *)(malloc(total_bytes));
+    if (p == NULL) return;
+    p->open_ended_array[10] = 0;
+}
+
+static void f_easier()
+{
+    size_t bytes = 5;
+    char *p = malloc(bytes);
+    if (p == NULL) return;
+    p[10] = 0;
+}
+
+static void f_easiest()
+{
+    char *p = malloc(5);
+    if (p == NULL) return;
+    p[10] = 0;
+}
+
+void main()
+{
+    f_easy1();
+    f_easy2();
+    f_easy3();
+    f_easy4();
+    f_easier();
+    f_easiest();
+}
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-default: structalloc.exe memcpy_test.exe memcpy_s_test.exe strncpy_s_test.exe leak.exe leak2.exe leak3.exe leak4.exe
+default: structalloc.exe memcpy_test.exe memcpy_s_test.exe strncpy_s_test.exe strncpy_s_test_unrolled.exe leak.exe leak2.exe leak3.exe leak4.exe buffer_overrun_101.exe
 
 structalloc.exe: structalloc.obj
 	link /out:$@ structalloc.obj
@@ -8,6 +8,8 @@ memcpy_s_test.exe: memcpy_s_test.obj
 	link /out:$@ memcpy_s_test.obj
 strncpy_s_test.exe: strncpy_s_test.obj
 	link /out:$@ strncpy_s_test.obj
+strncpy_s_test_unrolled.exe: strncpy_s_test_unrolled.obj
+	link /out:$@ strncpy_s_test_unrolled.obj
 leak.exe: leak.obj
 	link /out:$@ leak.obj
 leak2.exe: leak2.obj
@@ -16,6 +18,8 @@ leak3.exe: leak3.obj
 	link /out:$@ leak3.obj
 leak4.exe: leak4.obj
 	link /out:$@ leak4.obj
+buffer_overrun_101.exe: buffer_overrun_101.obj
+	link /out:$@ buffer_overrun_101.obj
 
 
 
@@ -27,6 +31,8 @@ memcpy_s_test.obj: memcpy_s_test.c
 	cl /c /W4 /analyze:WX- /FC memcpy_s_test.c
 strncpy_s_test.obj: strncpy_s_test.c
 	cl /c /W4 /analyze:WX- /FC strncpy_s_test.c
+strncpy_s_test_unrolled.obj: strncpy_s_test_unrolled.c
+	cl /c /W4 /analyze:WX- /FC strncpy_s_test_unrolled.c
 leak.obj: leak.c
 	cl /c /W4 /analyze:WX- /FC leak.c
 leak2.obj: leak2.c
@@ -35,6 +41,8 @@ leak3.obj: leak3.c
 	cl /c /W4 /analyze:WX- /FC leak3.c
 leak4.obj: leak4.c
 	cl /c /W4 /analyze:WX- /FC leak4.c
+buffer_overrun_101.obj: buffer_overrun_101.c
+	cl /c /W4 /analyze:WX- /FC buffer_overrun_101.c
 
 
 
